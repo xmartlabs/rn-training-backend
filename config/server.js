6bd43ecff0961bdcf60fbda65894b92475cca132
nicoache1 <nicoo.her@hotmail.com>
2021-05-10 12:42:27 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0e3467121fd0599234073c9c117756c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd8431022-81b5-4c7a-875f-00b16b1bb754'
+};
